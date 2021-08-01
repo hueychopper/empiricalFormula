d7033bb153ae23086e0b3c5361615b7c7a0105b8
hueychopper <75332544+hueychopper@users.noreply.github.com>
2021-08-01 13:14:26 -0400

    Update empiricalFormula.py

--- empiricalFormula.py
+++ empiricalFormula.py
@@ -23,44 +23,43 @@ def display_inputs(inputs):
     if(inputs > 0):
         for i in range(1, inputs + 1):
             elem_types = str(input("element name: "))
-            elem_mass = float(input("element mass: "))
-            percents = float(input("element percentage of mass: "))
+            percents = float(input("element percentage: "))
+            elem_molar_mass = float(input("element mass: "))
+            elem_mass = percents
 
-            if elem_types and elem_mass and percents:
+            if elem_types and percents and elem_molar_mass:
                 store_elem_name_mass.append(elem_types)
-                store_elem_name_mass.append(elem_mass)
+                store_elem_name_mass.append(elem_molar_mass)
                 store_elem_name_mass.append(percents)
-                #print(store_elem_name_mass)
-                mole_num = percents / elem_mass
+                
+                mole_num = elem_mass / elem_molar_mass
                 total = round(mole_num)
                 words = "It is"
                 if total:
-                    #mole_list.append(mole_num)
                     mole_list.append(total)
-                    print(f'{words} {mole_num} rounded is {total}'.format(mole_num, total))  
-
+                    print(f'{words} {mole_num} rounded is {total}')
                     mol_config = np.array(mole_list)
-                    final = min(mol_config)   
+                    final = min(mol_config)
+                print(final)
 
         def final_formula(name, subscript):
             #find all string in list
             res = len([val for val in store_elem_name_mass if isinstance(val, str)])
             #loop through and divide all moles by smallest
             for x in mol_config:
-                ur_moles = x / subscript
-                ronded = round(ur_moles)
+                moles = x / subscript
+                ronded = round(moles)
                 print(ronded)
-                mole_list.insert(0, ronded)
+                mole_list.append(moles)
+                #mole_list.insert(0, moles)
+                print(mole_list, "<--mole list")
                 try:
                     if res == 3:
                         mole_list.pop(3)
                 except IndexError:
                     print(mole_list)
-            print(ronded)
-
-            mole_list.sort()
-            print(store_elem_name_mass)
-            print(mole_list)
+            print(ronded, "<--rounded moles")
+            
         
             prompt = str(input("Would you like to see formula? [y/n]:  "))
             if prompt == "yes" or prompt == "y":
@@ -70,8 +69,10 @@ def display_inputs(inputs):
                         print(f'{store_elem_name_mass[0]}, {mole_list[0]}, {store_elem_name_mass[3]}, {mole_list[1]} is the formula')
                     elif res == 3:
                         print("is 3")
-                        print(mole_list)
-                        print(f'{store_elem_name_mass[0]}, {mole_list[0]}, {store_elem_name_mass[3]}, {mole_list[1]}, {store_elem_name_mass[6]}, {mole_list[2]} is your formula'.format())
+                        if 1 in mole_list:
+                            print(f'{store_elem_name_mass[0]}, {mole_list[0]}, {store_elem_name_mass[3]}, {mole_list[1]}, {store_elem_name_mass[6]}, {mole_list[2]} is your formula(you dont need to write the 1)')
+                        else:
+                            print(f'{store_elem_name_mass[0]}, {mole_list[0]}, {store_elem_name_mass[3]}, {mole_list[1]}, {store_elem_name_mass[6]}, {mole_list[2]} is your formula'.format())
                     elif res == 4:
                         print(f'{store_elem_name_mass[0]}, {mole_list[0]}, {store_elem_name_mass[3]}, {mole_list[1]}, {store_elem_name_mass[6]}, {mole_list[2]}, {store_elem_name_mass[9]}, {mole_list[3]}')
                     else:
